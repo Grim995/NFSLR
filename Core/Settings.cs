090e0028871d1628ea315c7b5f2bf647d4afca8d
Grim <grimmaple95@gmail.com>
2017-06-14 13:29:49 +0400

    Added Timer settings

---
+++ Core/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFSLR.Core
+{
+    class Settings : Options
+    {
+        [Property]
+        public int BkColor
+        {
+            get; set;
+        }
+
+        [Property]
+        public int TColor
+        {
+            get; set;
+        }
+
+        [Property]
+        public int StarHKey
+        {
+            get; set;
+        }
+
+        [Property]
+        public int StopHKey
+        {
+            get; set;
+        }
+
+        [Property]
+        public int ResetHKey
+        {
+            get; set;
+        }
+    }
+}
