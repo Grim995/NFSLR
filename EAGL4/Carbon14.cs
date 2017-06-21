5dc28ef786b05681ac1b129078900a79d8bb075f
Grim <grimmaple95@gmail.com>
2017-06-21 21:25:20 +0400

    NFSC14 logic added

---
+++ EAGL4/Carbon14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFSLR.EAGL4
+{
+    using Core;
+    public class Carbon14 : IGame
+    {
+        GameProcess proc;
+        bool isLoading;
+
+        protected int baseAddr;
+
+        public virtual bool Init(GameProcess process)
+        {
+            proc = process;
+            baseAddr = 0x00A9970C;
+            return true;
+        }
+
+        public void Update(GameTime sender, TimeEventArgs args)
+        {
+            isLoading = proc.ReadInt32Path(baseAddr, 0) != 0;
+            args.CountIn = !isLoading;
+        }
+    }
+}
