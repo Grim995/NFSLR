0320fa32d7f887cc68a8d160caed2db7c8e7b05c
Grim <grimmaple95@gmail.com>
2017-06-22 12:48:04 +0400

    Underground 2 Code added

---
+++ EAGL4/UG2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFSLR.EAGL4
+{
+    using Core;
+    public abstract class UG2 : IGame
+    {
+        protected int addr;
+        private GameProcess gameProcess;
+
+        private bool isLoading;
+        
+        public virtual bool Init(GameProcess process)
+        {
+            gameProcess = process;
+            return true;
+        }
+
+        public void Update(GameTime sender, TimeEventArgs args)
+        {
+            isLoading = gameProcess.ReadInt32(addr) != 2;
+            args.CountIn = !isLoading;
+        }
+    }
+}
---
+++ EAGL4/UG212EU.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFSLR.Core;
+
+namespace NFSLR.EAGL4
+{
+    public sealed class UG212EU : UG2
+    {
+        public UG212EU()
+        {
+
+        }
+
+        public override bool Init(GameProcess process)
+        {
+            addr = 0x00863774;
+            return base.Init(process);
+        }
+    }
+}
